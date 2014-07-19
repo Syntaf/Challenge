7b61bf3296f72038ea886c48afde8ada2cfe50a1
Grant Mercer <Gmercer015@gmail.com>
2014-07-19 13:00:00 +0100

    added basics for new challenge

---
+++ hexmap_rotation/main.rs
@@ -0,0 +1,16 @@
+/*
+  this program is a build off of the previous bitmap_ascii challenge. but to the next
+  level. We can read in an 8x8 picture from hex values. Once we have that image we can
+  start manipulating it
+
+  Zoom - zoom in or out of the image
+  Rotate - turn the image 90 degrees clockwise or counter clockwise
+  Invert - What was On is Off and what is Off becomes On. It inverts the image
+
+  reference: http://www.reddit.com/r/dailyprogrammer/comments/2avd5i/7162014_challenge_171_intermediate_zoom_rotate/
+*/
+
+
+fn main() {
+  println!("this is the start of hexmap_rotation");
+}
