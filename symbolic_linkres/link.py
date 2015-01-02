7b5f35a17d12f07bd270deb31302a046ee6307c0
Grant Mercer <Gmercer015@gmail.com>
2015-01-02 00:40:25 -0800

    finished symbolic link resolution program

--- symbolic_linkres/link.py
+++ symbolic_linkres/link.py
@@ -5,13 +5,36 @@ filename = input("Enter filename: ")
 #get data into string
 with open(filename) as f:
 		content = f.readlines()
+content = list(map(lambda s : s.strip('\n'), content))
 
 #the first line of our file is how many lines we need to read in
 num = int(content[0])
 #remove first element from content
 del content[0]
 
+#create list of links and names
+links = list()
+dirs = list()
+
 #run the loop NUM times to create symbolic links
 for i in range(0,num):
-	dirs = content[i].strip('\n').split(':')
-	print(dirs)
+	tmp_dirs = content[i].split(':')
+	#if not in your links ilst
+	if not tmp_dirs[0] in dirs:
+			dirs.append(tmp_dirs[0])
+			links.append(tmp_dirs[1])
+	else:
+			for h in range(0,len(dirs)):
+					if dirs[h] == tmp_dirs[0]:
+							links[h] = tmp_dirs[1]
+
+dir = content[-1]
+print(dir)
+for r in range(0,len(dirs)):
+		paths = dir[1:].split('/')
+		for i in range(len(paths), 0, -1):
+				if set(paths[:i]).issubset(dirs[r][1:].split('/')):
+						last = links[r] + "/" + "".join(paths[i])
+						break
+
+print(last)
