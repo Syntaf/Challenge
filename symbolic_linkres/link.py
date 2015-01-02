ac85f732c486fed16a2c4de18cc967600a658625
Grant Mercer <Gmercer015@gmail.com>
2015-01-02 00:44:35 -0800

    updated link file in symbolic resolution

--- symbolic_linkres/link.py
+++ symbolic_linkres/link.py
@@ -2,14 +2,13 @@
 
 filename = input("Enter filename: ")
 
-#get data into string
+#get data into string, strip newline characters
 with open(filename) as f:
 		content = f.readlines()
 content = list(map(lambda s : s.strip('\n'), content))
 
 #the first line of our file is how many lines we need to read in
 num = int(content[0])
-#remove first element from content
 del content[0]
 
 #create list of links and names
@@ -23,13 +22,17 @@ for i in range(0,num):
 	if not tmp_dirs[0] in dirs:
 			dirs.append(tmp_dirs[0])
 			links.append(tmp_dirs[1])
+	#else update links
 	else:
 			for h in range(0,len(dirs)):
 					if dirs[h] == tmp_dirs[0]:
 							links[h] = tmp_dirs[1]
 
+#get last element and print starting dir
 dir = content[-1]
 print(dir)
+#loop through all of our possible directories, establish where
+#our dir is and print the symbolic link
 for r in range(0,len(dirs)):
 		paths = dir[1:].split('/')
 		for i in range(len(paths), 0, -1):
