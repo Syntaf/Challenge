281b73a56059a071bdc84829107bbca405b16661
Grant Mercer <Gmercer015@gmail.com>
2015-05-08 20:21:00 -0700

    lazy typist work

--- lazy_typist/lazy.py
+++ lazy_typist/lazy.py
@@ -1,3 +1,20 @@
-from parse import board_parse
+from parse import board_parse, distance
 
 board = board_parse("board.dat")
+
+sentence = input("Enter a sentence(lower case / upper case no punctuation):\n")
+
+prev = (0,0)
+left_hand_pos = (3,5)
+right_hand_pos = (3,5)
+for letter in sentence:
+    for row in range(0,len(board)):
+        if letter in board[row]:
+            index = board[row].index(letter)
+
+    if distance((row,index),left_hand_pos) < distance((row,index),right_hand_pos):
+        print("left")
+        left_hand_pos = (row,index)
+    else:
+        print("right")
+        right_hand_pos = (row, index)
--- lazy_typist/parse.py
+++ lazy_typist/parse.py
@@ -1,5 +1,10 @@
+import math
+
 def board_parse(filename):
     with open(filename) as f:
         board = f.read().splitlines()   # split file into lines
 
     return [list(x) for x in board]     # turn each line into a list of chars
+
+def distance(p0, p1):
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
