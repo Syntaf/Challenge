7c0b0225771f53ac2de341d6e8254fb841c8bd07
Grant Mercer <Gmercer015@gmail.com>
2015-05-09 21:30:43 -0700

    finished lazy typist

--- lazy_typist/lazy.py
+++ lazy_typist/lazy.py
@@ -1,6 +1,9 @@
-from parse import board_parse, distance
-
+from parse import board_parse, distance, find_shift
 board = board_parse("board.dat")
+shifts = find_shift(board)
+lshift = shifts[0]
+rshift = shifts[1]
+
 
 sentence = input("Enter a sentence(lower case / upper case no punctuation):\n")
 
@@ -9,12 +12,24 @@ left_hand_pos = (3,5)
 right_hand_pos = (3,5)
 for letter in sentence:
     for row in range(0,len(board)):
-        if letter in board[row]:
-            index = board[row].index(letter)
-
-    if distance((row,index),left_hand_pos) < distance((row,index),right_hand_pos):
-        print("left")
-        left_hand_pos = (row,index)
+        if letter.lower() in board[row]:
+            index = board[row].index(letter.lower())
+    # if letter is uppercase
+    if letter == letter.upper():
+        if distance(right_hand_pos, rshift) < distance(left_hand_pos, lshift):
+            left_hand_pos = lshift
+            right_hand_pos = (row, index)
+            print("shift : left hand")
+            print(letter, ":     right hand")
+        else:
+            right_hand_pos = rshift
+            left_hand_pos = (row, index)
+            print("shift: right hand")
+            print(letter, ":     left hand")
     else:
-        print("right")
-        right_hand_pos = (row, index)
+        if distance((row,index),left_hand_pos) < distance((row,index),right_hand_pos):
+            print(letter, ":     left hand")
+            left_hand_pos = (row,index)
+        else:
+            print(letter, ":     right hand")
+            right_hand_pos = (row, index)
--- lazy_typist/parse.py
+++ lazy_typist/parse.py
@@ -8,3 +8,15 @@ def board_parse(filename):
 
 def distance(p0, p1):
     return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
+def find_shift(board):
+    shifts = []
+    for row in range(0,len(board)):
+        for col in range(0, len(board[row])):
+            if board[row][col] == '^':
+                shifts.append((row,col))
+    if distance(shifts[1], (0,0)) < distance(shifts[0], (0,0)):
+        a = shifts[0]
+        shifts[0] = shifts[1]
+        shifts[1] = a
+    return shifts
