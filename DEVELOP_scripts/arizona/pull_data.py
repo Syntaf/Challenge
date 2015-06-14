e3da10c05483d186e6fec509a6460c85bef68553
Syntaf <gmercer015@gmail.com>
2015-06-14 17:30:16 -0400

    finished script

--- DEVELOP_scripts/arizona/pull_data.py
+++ DEVELOP_scripts/arizona/pull_data.py
@@ -16,74 +16,42 @@ import string
 import shutil
 import subprocess
 import time
+import json
 from datetime import datetime, timedelta
 
-static_path_start = "http://api.mesowest.net/v2/stations/timeseries?token=0b1943fb6da945328d161ea6b64459ce&start="
+# The web address is split up into 3 seperate parts, we'll be injecting the start date, end date
+# and station ID to the address api/v2/stations/token&START=...&END=...&vars&STID= 
+static_path_start = "http://api.mesowest.net/v2/stations/timeseries?token=f11c52999e0144dcb4461f4fb3ef2b53&start="
 static_path_end = "05010000&end="
 static_path_STID = "10310000&vars=air_temp_high_24_hour,air_temp_low_24_hour,relative_humidity,sea_level_pressure&units=english&output=csv&obtime=local&STID="
 
-bad_chars = '(){}"<>[] '     # characers we want to strip from the string
-key_map = []
-
+# function to grab all station names from a text file, each appears on one line
 def getSTIDS(filename):
     with open(filename) as f:
+        # strip each line of any spaces or '\n' so we can directly append the element onto
+        # the web address
         stids = [x.translate(string.maketrans("", "", ), " \n") for x in f.readlines()]
     return stids
 
 # if the directory does not exist, create it
 stids = getSTIDS("stids.txt")
 
+# create the raw directory if it does not already exist
 if not os.path.exists("raw") : os.makedirs("raw")
-if not os.path.exists("formatted") : os.makedirs("formatted")
 
-# format file
-with open("format.dat") as f:
-    formatData = [x.strip('\n') for x in f.readlines()]
-
+# for each STATION in our STATIONS list
 for stid in stids:
+    # append the station name onto the "STID=" portion of the web address
     path_STID = static_path_STID + stid
-    # grab data for each day
+    # for each year we need data
     for date in range(2005,2015):
+        # wget <do not create directories> <output file=raw/date_stationName.dat>
+        # website address, with start and end date injected into the address
         proc_wget = "wget -nd -O " + \
-                "raw/" + str(date) + "_" + stid + ".dat " + \
+                "raw/" + str(date) + "_" + stid + ".dat " + "\"" + \
                 static_path_start + str(date) + \
                 static_path_end + str(date) + \
-                path_STID
+                path_STID + "\""
         
-        print proc_wget
-        """
-        p = subprocess.Popen(proc_wget, shell=True)
-        p.wait()
-        #os.system(proc_wget)
-        """
-        #time.sleep(.25)
-
-        """
-        # parse file
-        with open(os.path.join("raw", str(date) + "_" + stid + ".dat")) as f:
-            data = f.read()
-            data = data.strip('\n')
-            data = re.split('}|\[{', data)
-
-
-        data = filter(len, data)
-
-        # strip and split each station
-        for dat in data[1:-1]:
-            # perform black magic, don't even try to understand this
-            dat = dat.translate(string.maketrans("", "", ), bad_chars).split(',')
-            key_map.append(dict(x.split(':') for x in dat if ':' in x ))
-            if ':' not in dat[1]:key_map['NAME']+=dat[1]
-
-        print key_map
-        with open(os.path.join("formatted", str(date) + "_" + stid + ".dat"), 'w+') as of:
-            for station in range(0, len(key_map)):
-                for opt in formatData:
-                    of.write(opt + ":" + key_map[station][opt] + "\n")
-        """
-
-
-
-
-
-
+        # call wget using os.system()
+        os.system(proc_wget)
